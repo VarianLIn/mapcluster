e8b4e3a6f700f06753608ff320de4533d067a278
linbowen <varianlin18@163.com>
2020-09-04 10:25:07 +0800

    feat: 3d地球动态效果图层 --varianlin

--- src/views/mainpage/3dmap/3dGlobe.vue
+++ src/views/mainpage/3dmap/3dGlobe.vue
@@ -40,33 +40,29 @@ export default {
             //代理服务地址
             Cfg.proxyHostUrl =
                 "http://" + Cfg.host + "/" + Cfg.projectName + "/proxy?url=";
-            var viewer = new Cesium.Map("globeContainer");
+
+            // 加载地球
+            let viewer = new Cesium.Map("globeContainer", {
+                infoBox: false, //信息框
+                selectionIndicator: false, //选择指示器
+            });
+            viewer.scene.globe.depthTestAgainstTerrain = true; //设置元素真实抵靠地形地面
 
             //加载天地图web墨卡托的全球影像
-            // var tdt = new Cesium.UrlTemplateImageryProvider({
-            //     url:
-            //         "http://{s}.tianditu.com/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=e90d56e5a09d1767899ad45846b0cefd",
-            //     subdomains: ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7"],
-            // });
-            //加载天地图经纬度的全球影像
+            var tdt = new Cesium.UrlTemplateImageryProvider({
+                url:
+                    "http://{s}.tianditu.com/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=e90d56e5a09d1767899ad45846b0cefd",
+                subdomains: ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7"],
+            });
+            viewer.imageryLayers.addImageryProvider(tdt);
+
+            // // 加载天地图经纬度的全球影像
             // var tdt = new Cesium.UrlTemplateImageryProvider({
             // 	url: "http://{s}.tianditu.com/DataServer?T=img_c&x={x}&y={y}&l={z}&tk=e90d56e5a09d1767899ad45846b0cefd",
             // 	subdomains: ['t0','t1','t2','t3','t4','t5','t6','t7'],
             // 	tilingScheme: new Cesium.GeoGeographicTilingScheme()
             // });
             // viewer.imageryLayers.addImageryProvider(tdt);
-
-            // mapbox底图
-            var layer = new Cesium.MapboxImageryProvider({
-                // mapId: "mapbox.dark",
-                // accessToken:
-                //     "pk.eyJ1IjoidmFyaWFubGluIiwiYSI6ImNrY3EyNzUwYTEwczAycmx1Zzc5dTlkM20ifQ.NE25cXftMqiajLsD5sqHkg",
-                url: "http://b.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
-                accessToken:
-                    "pk.eyJ1IjoidmFyaWFubGluIiwiYSI6ImNrY3EyNzUwYTEwczAycmx1Zzc5dTlkM20ifQ.NE25cXftMqiajLsD5sqHkg",
-                styleId: "ck5290o2z121u1cle7mdtfmdk",
-            });
-            viewer.imageryLayers.addImageryProvider(layer);
 
             // //开启天地图卫星影像的色差特殊处理， 如非必须请关闭该功能！
             // viewer.camera.isTDT = true;
@@ -75,18 +71,17 @@ export default {
             //     -30
             // );
 
-            // var viewModel = {
-            //     x: 0,
-            // };
-            // gui = new dat.GUI({});
-            // var f = gui.addFolder("视图控制");
-
-            // gui.domElement.id = "gui";
-
-            // f.add(viewModel, "x", 0, 10, 0.001).onChange(function (value) {
-            //     // console.log(value);
+            // // mapbox底图
+            // var mapboxlayer = new Cesium.MapboxImageryProvider({
+            //     mapId: "mapbox.dark",
+            //     url: "http://b.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
+            //     accessToken:
+            //         "pk.eyJ1IjoidmFyaWFubGluIiwiYSI6ImNrY3EyNzUwYTEwczAycmx1Zzc5dTlkM20ifQ.NE25cXftMqiajLsD5sqHkg",
+            //     styleId: "ck5290o2z121u1cle7mdtfmdk",
             // });
-            // f.open();
+            // viewer.imageryLayers.addImageryProvider(mapboxlayer);
+
+            // 动态柱体
             var dynamicCylinder = new Cesium.GeoDynamicCylinder({
                 viewer: viewer,
                 center: [108.9587516378286, 34.21978169947116, 0],
@@ -97,87 +92,93 @@ export default {
                 bottomRadius: 10,
                 particlesImageUrl: "./static/jpg/取消.png",
             });
-            // viewer.camera.flyTo({
-            //     destination: Cesium.Cartesian3.fromDegrees(
-            //         108.9587516378286,
-            //         34.21978169947116,
-            //         450
-            //     ),
-            //     orientation: {
-            //         heading: 0,
-            //         pitch: -1,
-            //         roll: 0.0008812885117928104,
-            //     },
-            // });
-            viewer.zoomTo([108.9587516378286, 34.21978169947116, 450]);
-            // //得到当前三维场景
-            // var scene = viewer.scene;
-            // //得到当前三维场景的椭球体
-            // var ellipsoid = scene.globe.ellipsoid;
-            // var longitudeString = null;
-            // var latitudeString = null;
-            // var height = null;
-            // var cartesian = null;
+            viewer.camera.flyTo({
+                destination: Cesium.Cartesian3.fromDegrees(
+                    108.9587516378286,
+                    34.21978169947116,
+                    450
+                ),
+                orientation: {
+                    heading: 0,
+                    pitch: -1,
+                    roll: 0.0008812885117928104,
+                },
+            });
+            // viewer.zoomTo(viewer.entities);
 
-            // var handler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
-            // console.log(handler);
-            // //获取鼠标点击时的坐标
-            // getPosition(Cesium.ScreenSpaceEventType.LEFT_CLICK);
-            // function getPosition(actionType) {
-            //     //移除所有事件
-            //     handler.removeInputAction(
-            //         Cesium.ScreenSpaceEventType.LEFT_CLICK
-            //     );
-            //     handler.removeInputAction(
-            //         Cesium.ScreenSpaceEventType.MIDDLE_CLICK
-            //     );
-            //     handler.removeInputAction(
-            //         Cesium.ScreenSpaceEventType.RIGHT_CLICK
-            //     );
-            //     handler.removeInputAction(Cesium.ScreenSpaceEventType.WHEEL);
-            //     if (actionType !== Cesium.ScreenSpaceEventType.WHEEL) {
-            //         //设置鼠标点击事件的处理函数，这里负责监听x,y坐标值变化
-            //         handler.setInputAction(function (click) {
-            //             //通过指定的椭球或者地图对应的坐标系，将鼠标的二维坐标转换为对应椭球体三维坐标
-            //             cartesian = viewer.camera.pickEllipsoid(
-            //                 click.position,
-            //                 ellipsoid
-            //             );
-            //             //将笛卡尔坐标转换为地理坐标
-            //             var cartographic = ellipsoid.cartesianToCartographic(
-            //                 cartesian
-            //             );
-            //             //将弧度转为度的十进制度表示
-            //             longitudeString = Cesium.Math.toDegrees(
-            //                 cartographic.longitude
-            //             );
-            //             latitudeString = Cesium.Math.toDegrees(
-            //                 cartographic.latitude
-            //             );
-            //             //获取相机高度
-            //             height = Math.ceil(
-            //                 viewer.camera.positionCartographic.height
-            //             );
-            //             positionInfo.setValue(
-            //                 longitudeString.toFixed(3) +
-            //                     "," +
-            //                     latitudeString.toFixed(3) +
-            //                     "," +
-            //                     height.toFixed(3)
-            //             );
-            //         }, actionType);
-            //     } else {
-            //         //设置鼠标滚动事件的处理函数，这里负责监听高度值变化
-            //         handler.setInputAction(function (wheelment) {
-            //             height = Math.ceil(
-            //                 viewer.camera.positionCartographic.height
-            //             );
-            //             positionInfo.setValue(
-            //                 "事件触发时相机高度：" + height.toFixed(3) + "m"
-            //             );
-            //         }, actionType);
-            //     }
-            // } //getposition
+            //得到当前三维场景
+            var scene = viewer.scene;
+            //得到当前三维场景的椭球体
+            var ellipsoid = scene.globe.ellipsoid;
+            var longitudeString = null;
+            var latitudeString = null;
+            var height = null;
+            var cartesian = null;
+
+            var handler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
+            console.log(handler);
+            //获取鼠标点击时的坐标
+            getPosition(Cesium.ScreenSpaceEventType.LEFT_CLICK);
+            function getPosition(actionType) {
+                //移除所有事件
+                handler.removeInputAction(
+                    Cesium.ScreenSpaceEventType.LEFT_CLICK
+                );
+                handler.removeInputAction(
+                    Cesium.ScreenSpaceEventType.MIDDLE_CLICK
+                );
+                handler.removeInputAction(
+                    Cesium.ScreenSpaceEventType.RIGHT_CLICK
+                );
+                handler.removeInputAction(Cesium.ScreenSpaceEventType.WHEEL);
+                if (actionType !== Cesium.ScreenSpaceEventType.WHEEL) {
+                    //设置鼠标点击事件的处理函数，这里负责监听x,y坐标值变化
+                    handler.setInputAction(function (click) {
+                        //通过指定的椭球或者地图对应的坐标系，将鼠标的二维坐标转换为对应椭球体三维坐标
+                        cartesian = viewer.camera.pickEllipsoid(
+                            click.position,
+                            ellipsoid
+                        );
+                        //将笛卡尔坐标转换为地理坐标
+                        var cartographic = ellipsoid.cartesianToCartographic(
+                            cartesian
+                        );
+                        //将弧度转为度的十进制度表示
+                        longitudeString = Cesium.Math.toDegrees(
+                            cartographic.longitude
+                        );
+                        latitudeString = Cesium.Math.toDegrees(
+                            cartographic.latitude
+                        );
+                        //获取相机高度
+                        height = Math.ceil(
+                            viewer.camera.positionCartographic.height
+                        );
+                        // positionInfo.setValue(
+                        //     longitudeString.toFixed(3) +
+                        //         "," +
+                        //         latitudeString.toFixed(3) +
+                        //         "," +
+                        //         height.toFixed(3)
+                        // );
+                        console.log(
+                            longitudeString.toFixed(3),
+                            latitudeString.toFixed(3),
+                            height.toFixed(3)
+                        );
+                    }, actionType);
+                } else {
+                    //设置鼠标滚动事件的处理函数，这里负责监听高度值变化
+                    handler.setInputAction(function (wheelment) {
+                        height = Math.ceil(
+                            viewer.camera.positionCartographic.height
+                        );
+                        // positionInfo.setValue(
+                        //     "事件触发时相机高度：" + height.toFixed(3) + "m"
+                        // );
+                    }, actionType);
+                }
+            } //getposition
         }, // init
 
         // addgui() {
--- src/views/mainpage/3dmap/3dmigrationMap.vue
+++ src/views/mainpage/3dmap/3dmigrationMap.vue
@@ -23,22 +23,48 @@ export default {
     },
     methods: {
         init() {
-            var viewer = new Cesium.Map("threedmigration");
+            //地球
+            let viewer = new Cesium.Map("threedmigration", {
+                infoBox: false,
+                selectionIndicator: false, //选择指示器
+            });
+            viewer.scene.globe.depthTestAgainstTerrain = true; //设置元素真实抵靠地形地面
 
+            // //地形
+            // let terrainUrls = [
+            //     "https://t8.tianditu.gov.cn/DEM/DataServer?T=ele_c&tk=e90d56e5a09d1767899ad45846b0cefd",
+            //     "https://t9.tianditu.gov.cn/DEM/DataServer?T=ele_c&tk=e90d56e5a09d1767899ad45846b0cefd",
+            //     "https://t10.tianditu.gov.cn/DEM/DataServer?T=ele_c&tk=e90d56e5a09d1767899ad45846b0cefd",
+            //     "https://t11.tianditu.gov.cn/DEM/DataServer?T=ele_c&tk=e90d56e5a09d1767899ad45846b0cefd",
+            //     "https://t12.tianditu.gov.cn/DEM/DataServer?T=ele_c&tk=e90d56e5a09d1767899ad45846b0cefd",
+            // ];
+            // let t_Provider = new Cesium.GeoTerrainProvider({
+            //     dataType: Cesium.GeoTerrainProvider.INT,
+            //     urls: terrainUrls,
+            // });
+            // viewer.terrainProvider = t_Provider;
+            //天地图影像
+            // viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
+            // let tdt = new Cesium.UrlTemplateImageryProvider({
+            //     url:
+            //         "http://{s}.tianditu.com/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=e90d56e5a09d1767899ad45846b0cefd",
+            //     subdomains: ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7"],
+            //     maximumLevel: 18,
+            // });
+            // viewer.imageryLayers.addImageryProvider(tdt);
+
+            // mapbox地图 黑暗
             var layer = new Cesium.MapboxImageryProvider({
-                // mapId: "mapbox.dark",
-                // accessToken:
-                //     "pk.eyJ1IjoidmFyaWFubGluIiwiYSI6ImNrY3EyNzUwYTEwczAycmx1Zzc5dTlkM20ifQ.NE25cXftMqiajLsD5sqHkg",
+                mapId: "mapbox.dark",
                 url:
                     "http://b.basemaps.cartocdn.com/spotify_dark/{z}/{x}/{y}.png",
                 accessToken:
                     "pk.eyJ1IjoidmFyaWFubGluIiwiYSI6ImNrY3EyNzUwYTEwczAycmx1Zzc5dTlkM20ifQ.NE25cXftMqiajLsD5sqHkg",
                 styleId: "ck5290o2z121u1cle7mdtfmdk",
             });
-
             viewer.imageryLayers.addImageryProvider(layer);
 
-            //设置最大俯仰角，[-90,0]区间内，默认为-30，单位弧度
+            // // 设置最大俯仰角，[-90,0]区间内，默认为-30，单位弧度
             // viewer.scene.screenSpaceCameraController.constrainedPitch = Cesium.Math.toRadians(
             //     -30
             // );
@@ -57,10 +83,10 @@ export default {
 
             var coor = [
                 [114.11085388187564, 30.86173327022155, 500],
-                [114.61416503910482, 30.868806070806684, 500],
+                [114.61416503910482, 30.86880607080668, 500],
                 [114.70480224612243, 30.55888400498388, 500],
-                [114.2674090576574, 30.590807618525005, 500],
-            ];
+                [114.26740905765741, 30.59080761852511, 500],
+            ]; //coor
             var data = [
                 {
                     //飞线路径：起始点，终结点
@@ -110,30 +136,90 @@ export default {
                 isParabola: true,
             }); // odline
 
+            // 四周 动态柱体
             coor.forEach((element) => {
                 var dynamicCylinder = new Cesium.GeoDynamicCylinder({
                     viewer: viewer,
                     center: element,
-                    particlesColor: Cesium.Color.GOLD,
-                    cylinderColor: Cesium.Color.GOLD,
+                    // particlesColor: Cesium.Color.GOLD,
+                    cylinderColor: new Cesium.Color(10, 10, 0, 10),
                     length: 2000,
-                    topRadius: 200,
+                    topRadius: 150,
                     bottomRadius: 400,
                     // particlesImageUrl: "./static/jpg/取消.png",
                 });
             });
-            // var dynamicCylinder = new Cesium.GeoDynamicCylinder({
-            //     viewer: viewer,
-            //     center: [114.345, 30.675, 500],
-            //     particlesColor: Cesium.Color.RED,
-            //     cylinderColor: Cesium.Color.RED,
-            //     length: 5000,
-            //     topRadius: 200,
-            //     bottomRadius: 400,
-            //     // particlesImageUrl: "./static/jpg/取消.png",
-            // });
+            // 四周 动态圆圈
+            coor.forEach((element) => {
+                var dynamicCircle = new Cesium.GeoDynamicCircle({
+                    viewer: viewer,
+                    center: element,
+                    height: 0,
+                    color: Cesium.Color.AQUA,
+                    radius: 800,
+                });
+            });
 
-            //
+            // 中心点 动态圆圈
+            var dynamicCircle1 = new Cesium.GeoDynamicCircle({
+                viewer: viewer,
+                center: [114.345, 30.675, 100],
+                height: 0,
+                color: Cesium.Color.RED,
+                radius: 1500,
+            });
+            // 中心点 动态柱体
+            var dynamicCylinder = new Cesium.GeoDynamicCylinder({
+                viewer: viewer,
+                center: [114.345, 30.675, 2000],
+                // particlesColor: Cesium.Color.RED,
+                cylinderColor: new Cesium.Color(1, 0, 0, 10),
+                length: 5000,
+                topRadius: 150,
+                bottomRadius: 800,
+                // particlesImageUrl: "./static/jpg/取消.png",
+            });
+
+            //中心点 动态柱体图层
+            // var cylinderEffectLayer = new Cesium.GeoCylinderEffectLayer({
+            //     effectTypeMapField: "effectType",
+            //     // dataSource: "",
+            //     color: Cesium.Color.RED,
+            //     colorMapField: "color",
+            //     radius: 1000.0,
+            //     radiusMapField: "radius",
+            //     height: 0,
+            //     heightMapField: "height",
+            //     length: 5000,
+            //     lengthMapField: "length",
+            //     topRadius: 300,
+            //     topRadiusMapField: "topRadius",
+            //     bottomRadius: 400,
+            //     bottomRadiusMapField: "bottomRadius",
+            //     particlesColor: Cesium.Color.GOLD,
+            //     particlesColorMapField: "particlesColor",
+            //     cylinderColor: Cesium.Color.GOLD,
+            //     cylinderColorMapField: "cylinderColor",
+            //     // particlesImageUrl:
+            //     //     "/geomap-api/JsCesuimDemo/resource/images/particles.png",
+            // });
+            // cylinderEffectLayer.addTo(viewer);
+            // cylinderEffectLayer.setData([
+            //     {
+            //         type: "Feature",
+            //         properties: {
+            //             name: "Cylinder1",
+            //             particlesColor: "rgba(0,0,255,1)",
+            //             cylinderColor: "rgba(0,0,255,1)",
+            //         },
+            //         geometry: {
+            //             type: "Point",
+            //             coordinates: [114.345, 30.675, 0],
+            //         },
+            //     },
+            // ]);//中心点 动态柱体图层
+
+            // 动态墙
             // var scene = viewer.scene;
             // var fireEffect = new Cesium.GeoCylinderApertureSpreadEffect({
             //     color: Cesium.Color.WHITE,
@@ -145,26 +231,18 @@ export default {
             // });
             // scene.primitives.add(fireEffect);
 
-            //
-            var dynamicCircle1 = new Cesium.GeoDynamicCircle({
-                viewer: viewer,
-                center: [114.345, 30.675, 500],
-                height: 0,
-                color: Cesium.Color.GOLD,
-                radius: 1500,
-            });
-
-            var redBox = viewer.entities.add({
-                name: "Red box with black outline",
-                position: Cesium.Cartesian3.fromDegrees(114.345, 30.675, 0),
-                box: {
-                    dimensions: new Cesium.Cartesian3(400.0, 300.0, 1500.0),
-                    material: Cesium.Color.RED.withAlpha(0.5),
-                    outline: true,
-                    outlineColor: Cesium.Color.BLACK,
-                },
-            });
-
+            // cesium实体
+            // var redBox = viewer.entities.add({
+            //     name: "Red box with black outline",
+            //     position: Cesium.Cartesian3.fromDegrees(114.345, 30.675, 0),
+            //     box: {
+            //         dimensions: new Cesium.Cartesian3(400.0, 300.0, 1500.0),
+            //         material: Cesium.Color.RED.withAlpha(0.5),
+            //         outline: true,
+            //         outlineColor: Cesium.Color.BLACK,
+            //     },
+            // });
+            console.log(viewer.entities);
             viewer.zoomTo(viewer.entities);
         }, // init
     },
